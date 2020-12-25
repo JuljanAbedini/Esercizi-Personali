1da5d3cb713257c4d4e3dda206467fc24d65880e
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-25 12:06:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Variables
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            string surname = "McCain";
+            Console.WriteLine("Hello " + name + " " + surname);
+
+            Console.WriteLine("Variables ");
+           
+            int myNum = 5;                  //intiger (whole number)
+            double mydouble = 5.5;          // floating point number
+            char myChar = 'J';              // character
+            string myString = "Abedini";    // string
+            bool myBool = true;            // boolean
+
+            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
+
+            Console.WriteLine("Implicit Casting");
+            int myInt = 9;                              
+            double myDouble = myInt;
+            Console.WriteLine(myInt + "\n" + myDouble);
+
+
+
+            
+
+
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
