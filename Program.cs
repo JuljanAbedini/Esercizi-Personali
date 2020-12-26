7f11dff2be31c023ab39be13e06fcfbf1eac86c5
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:51:27 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,113 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Variables
-
+namespace Dati_Personali
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            string surname = "McCain";
-            Console.WriteLine("Hello " + name + " " + surname);
-
-            Console.WriteLine("Variables ");
            
-            int myNum = 5;                  //intiger (whole number)
-            double mydouble = 5.5;          // floating point number
-            char myChar = 'J';              // character
-            string myString = "Abedini";    // string
-            bool myBool = true;            // boolean
+            Console.WriteLine("Welcome!");
+            Console.ReadLine();
+            Console.WriteLine("Give your Personal Details:");
+            Console.ReadLine();
+            Console.WriteLine("What is your Name?");
+            string Name = Console.ReadLine();
+            
+            Console.ReadLine();
+            Console.WriteLine("What is your Surname?");
+            string Surname = Console.ReadLine();
+            
+            Console.ReadLine();
+            Console.WriteLine("Place of Birth?");
+            string Birth = Console.ReadLine();
+            
+            Console.ReadLine();
+            Console.WriteLine("Date of Birth?");
+            string Date = Console.ReadLine();
+            
+            Console.ReadLine();
+            Console.WriteLine("Where do you Live?");
+            string Live = Console.ReadLine();
+            
+            Console.ReadLine();
+            Console.WriteLine("What day is Today?");
+            string ToDay = Console.ReadLine();
+            
 
-            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
 
-            Console.WriteLine("Implicit Casting");
-            int myInt = 9;                              
-            double myDouble = myInt;
-            Console.WriteLine(myInt + "\n" + myDouble);
+            Console.ReadKey();
+           
+            
 
+
+
+
+
+            /*
+            
+            Console.WriteLine("Welcome!");
+            Console.ReadLine();
+            Console.WriteLine("Name:");
+            Console.ReadLine();
+            Console.WriteLine("Juljan");
+            Console.ReadLine();
+            Console.WriteLine("Surname:");
+            Console.ReadLine();
+            Console.WriteLine("Abedini");
+            Console.ReadLine();
+            Console.WriteLine("Date of Birth:");
+            Console.ReadLine();
+            Console.WriteLine("17.March.1984");
+            Console.ReadLine();
+            Console.WriteLine("Place of Birth:");
+            Console.ReadLine();
+            Console.WriteLine("ALBANIA");
 
 
             
 
+            Console.ReadLine();
+            */
 
+
+
+
+            /*
            
+            Console.WriteLine("Welcome!");
+
+            Console.WriteLine("Give your Personal Details");
+            Console.ReadLine();
+            Console.WriteLine("What is your Name?");
+            string Name = Console.ReadLine();
+            Console.WriteLine(Name);
+            Console.ReadLine();
+            Console.WriteLine("What is your Surname?");
+            string Surname = Console.ReadLine();
+            Console.WriteLine(Surname);
+            Console.ReadLine();
+            Console.WriteLine("Place of Birth?");
+            string Birth = Console.ReadLine();
+            Console.WriteLine(Birth);
+            Console.ReadLine();
+            Console.WriteLine("Date of Birth?");
+            string Date = Console.ReadLine();
+            Console.WriteLine(Date);
+            Console.ReadLine();
+            Console.WriteLine("Where do you Live?");
+            string Live = Console.ReadLine();
+            Console.WriteLine(Live);
+            Console.ReadLine();
+            Console.WriteLine("What day is Today?");
+            string ToDay = Console.ReadLine();
+            Console.WriteLine(ToDay);
 
 
-            Console.ReadKey();
+            Console.ReadKey();     */
         }
     }
 }
