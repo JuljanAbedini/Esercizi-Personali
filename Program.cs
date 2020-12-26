356c0720ac29be0cf12d3a3d31b25e5d148a9272
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:53:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Variables
-
+namespace Prova_Generale
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            string surname = "McCain";
-            Console.WriteLine("Hello " + name + " " + surname);
-
-            Console.WriteLine("Variables ");
-           
-            int myNum = 5;                  //intiger (whole number)
-            double mydouble = 5.5;          // floating point number
-            char myChar = 'J';              // character
-            string myString = "Abedini";    // string
-            bool myBool = true;            // boolean
-
-            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
-
-            Console.WriteLine("Implicit Casting");
-            int myInt = 9;                              
-            double myDouble = myInt;
-            Console.WriteLine(myInt + "\n" + myDouble);
-
-
-
-            
-
-
-           
-
-
-            Console.ReadKey();
+            Console.WriteLine("Nome: Juljan");
+            Console.ReadLine();
+            Console.WriteLine("Cognome: ABEDINI");
+            Console.ReadLine();
+            Console.WriteLine("Data di Nascita: 17.Marzo.1984");
+            Console.ReadLine();
+            Console.WriteLine("Luogo di Nascita: Durres, Albania");
+            Console.ReadLine();                         
+            Console.WriteLine("Residenza: Moncalieri");
+            Console.ReadLine();                         
+            Console.WriteLine("Cittadinanza: ALBANESE");
+            Console.ReadLine();
         }
     }
 }
