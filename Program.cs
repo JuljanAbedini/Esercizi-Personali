e463a02f88a6369cd62d96eb1d9c79ada639f64a
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:53:49 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,75 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Variables
-
+namespace Variabili
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            string surname = "McCain";
-            Console.WriteLine("Hello " + name + " " + surname);
+            Console.WriteLine("Buonasera!");
 
-            Console.WriteLine("Variables ");
-           
-            int myNum = 5;                  //intiger (whole number)
-            double mydouble = 5.5;          // floating point number
-            char myChar = 'J';              // character
-            string myString = "Abedini";    // string
-            bool myBool = true;            // boolean
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Juljan";
+            bool isWorking = false;
 
-            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
-
-            Console.WriteLine("Implicit Casting");
-            int myInt = 9;                              
-            double myDouble = myInt;
-            Console.WriteLine(myInt + "\n" + myDouble);
-
-
-
-            
-
-
-           
-
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
 
             Console.ReadKey();
+            
+            
+            
+            
+            
+            
+            
+        
+            
+            
+            /* Console.WriteLine("Benvenuto!");
+
+            Console.WriteLine("Quall'è il tuo nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine(nome);
+            Console.ReadLine();
+
+            Console.WriteLine("Che giorno è oggi?");
+           
+            string giorno = Console.ReadLine();
+            Console.WriteLine(giorno);
+            Console.ReadLine();
+
+
+            Console.ReadKey();  */
+              
+
+
+
+
+           /* Console.WriteLine("Benvenuto!");
+           
+            Console.WriteLine("Come ti chiami?");
+            string inputUtente = Console.ReadLine();
+            Console.WriteLine(inputUtente); */
+
+
+           /* string name = "Juljan";
+            Console.WriteLine(name);
+
+            string inputUtente = Console.ReadLine();
+            Console.WriteLine(inputUtente);*/
+
+            
         }
     }
 }
