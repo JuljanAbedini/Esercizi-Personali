4f17d376ea504a0eda8db36587b494bd11b26323
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:56:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,27 @@
 ﻿using System;
 
-namespace Variables
-
+namespace ConsoleAppUserImput
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            string surname = "McCain";
-            Console.WriteLine("Hello " + name + " " + surname);
+            Console.WriteLine("Enter Username e Password!");
+            Console.WriteLine("Enter Username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
 
-            Console.WriteLine("Variables ");
-           
-            int myNum = 5;                  //intiger (whole number)
-            double mydouble = 5.5;          // floating point number
-            char myChar = 'J';              // character
-            string myString = "Abedini";    // string
-            bool myBool = true;            // boolean
-
-            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
-
-            Console.WriteLine("Implicit Casting");
-            int myInt = 9;                              
-            double myDouble = myInt;
-            Console.WriteLine(myInt + "\n" + myDouble);
+            Console.WriteLine("Enter Password");
+            string passWord = Console.ReadLine();
+            string Password = "J17031984a";
+            Console.WriteLine("Password is: " + passWord);
 
 
-
+            //User Input and Numbers
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
             
-
-
-           
 
 
             Console.ReadKey();
