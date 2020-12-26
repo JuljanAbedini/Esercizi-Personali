d65c51a5238a7bc7465ca8c3bab807f46c0f74fe
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:55:58 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,110 @@
 ﻿using System;
 
-namespace Variables
-
+namespace Strings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            string surname = "McCain";
-            Console.WriteLine("Hello " + name + " " + surname);
+            /*
+            string greeting = "Hello World!";
+            Console.WriteLine(greeting);
 
-            Console.WriteLine("Variables ");
-           
-            int myNum = 5;                  //intiger (whole number)
-            double mydouble = 5.5;          // floating point number
-            char myChar = 'J';              // character
-            string myString = "Abedini";    // string
-            bool myBool = true;            // boolean
+            //Length
 
-            Console.WriteLine(myNum + "\n" + mydouble + "\n" + myChar + "\n" + myString + "\n" + myBool);
+            string txt = "hshdfjhhsdfjkhdsfjdjfdsjgfhfdsj";
+            Console.WriteLine(txt.ToUpper());
+            Console.WriteLine("the length must be only words: " + txt.Length);
 
-            Console.WriteLine("Implicit Casting");
-            int myInt = 9;                              
-            double myDouble = myInt;
-            Console.WriteLine(myInt + "\n" + myDouble);
+            // ToUpper ToLower
+            string words = "Hi Juli";
+            Console.WriteLine(words.ToUpper());
+            Console.WriteLine(words.ToLower());
+
+            //String Concatenation
+            string firstName = "Juljan";
+            string lastname = "Abedini";
+            string name = firstName + " " + lastname;
+            Console.WriteLine(name.ToUpper());
+
+            string primoNome = "Giorgio";
+            string cognome = "Rossi";
+            string nome = string.Concat(primoNome, " ", cognome);
+            Console.WriteLine(nome.ToLower());
+            */
+
+            //String Interpolation
+            /*
+            string firstName = "Joel";
+            string lastName = "Abedini";
+            string name = $"{firstName} {lastName}";
+            Console.WriteLine(name.ToUpper());
+            */
+
+            //Access Strings
+
+            string myString = "Hello";
+            Console.WriteLine(myString.ToUpper()[0]);
+            Console.WriteLine(myString.ToUpper()[1]);
+            Console.WriteLine(myString.ToUpper()[2]);
+            Console.WriteLine(myString.ToUpper()[3]);
+            Console.WriteLine(myString.ToUpper()[4]);
+          
+            //OR
+            Console.WriteLine(myString.IndexOf("e"));
+
+            //Substring
+
+            //Full Name
+            string name = "Juljan Abedini";
+
+            //location of letter A
+            int charPos = name.IndexOf("A");
+
+            //get last name
+            string lastName = name.Substring(charPos);
+
+            Console.WriteLine(lastName.ToUpper());
 
 
+            //Special Characters:
 
-            
+            // \"
+            string txt = "Ne jemi \"CAME\" dhe vim nga Jugu i Shqiperise. ";
+            Console.WriteLine(txt);
 
+            // \'
+            string word = "It\'s alright.";
+            Console.WriteLine(word);
 
-           
+            //   \\
+            string wordOne = "The character \\ is called backslash.";
+            Console.WriteLine(wordOne);
 
+            // \n   	New Line
+            string wordtwo = "Tirane \nAlbania";
+            Console.WriteLine(wordtwo);
+
+            // \t   	Tab
+            string wordThree = "Tirane \tAlbania";
+            Console.WriteLine(wordThree);
+
+            // \b   Backspace
+            string wordFour = "Tirane \bAlbania";
+            Console.WriteLine(wordFour);
+
+            // C# uses the + operator for both addition and concatenation.
+            // Remember: Numbers are added.Strings are concatenated.
+
+            int x = 10;
+            int y = 15;
+            int z = x + y; // // z will be 30 (an integer/number)
+            Console.WriteLine(z);
+
+            string a = "10";
+            string b = "25";
+            string c = a + b; // // z will be 1020 (a string)
+            Console.WriteLine(c);
 
             Console.ReadKey();
         }
